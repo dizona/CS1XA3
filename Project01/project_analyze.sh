60aaf722e5132a4f22baaeec96d870d32ae2eb19
Adrian Dizon <dizona@mcmaster.ca>
2019-02-15 03:19:22 +0000

    Finished #TODO function/Bug fixes for #TODO function

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 #all files are relative to CS1XA3
 cd ..
-
-#Checks if todo.log exitsts. If it does delete and create a new one.
-if [ -f ./Project01/todo.log ]; then
-	rm ./Project01/todo.log
-else
-	touch ./Project01/todo.log
-fi
-
+#read -p "Welcome! Enter: \n 1 For #TODO function" 
 #Checks every files in repo
 for userInput in "$@"
 do
-  	if [ "$userInput" = "todo-log" ]; then
+  	if [ "$userInput" = "todo" ]; then
 		if [ -f ./Project01/todo.log ]; then
 			rm ./Project/01/todo.log
 		else
