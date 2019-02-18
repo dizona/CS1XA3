e4a81f9193997726e6d37d219428cc8fd9822620
Adrian Dizon <dizona@mcmaster.ca>
2019-02-18 05:23:20 +0000

    Finished file count funtion for project_analyze

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#i!/bin/bash
 #all files are relative to CS1XA3
 cd ..
 #read -p "Welcome! Enter: \n 1 For #TODO function" 
@@ -13,7 +13,23 @@ do
 		fi
 		
 		grep -r --exclude={project_analyze.sh,todo.log} --exclude-dir=.git "#TODO" ./ >> ./Project01/todo.log
+	
+	elif [ "$userInput" = "FileCount" ]; then
+		
+		html=$(find ./ -iname "*.html" | wc -l)
+		echo "HTML: $html"
+		javaScript=$(find ./ -iname "*.js" | wc -l)
+		echo "JavaScript: $javaScript"
+		css=$(find ./ -iname "*.css" | wc -l)
+		echo "CSS: $css"
+		python=$(find ./ -iname "*.py" | wc -l)
+		echo "Python: $python"
+		haskell=$(find ./ -iname "*.hs" | wc -l)
+		echo "Haskell: $haskell" 
+		bash=$(find ./ -iname "*sh" | wc -l)
+		echo "Bash: $bash"
 
 	fi
+
 done
 
